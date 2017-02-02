beea5fbf5826ade9358b887ded8c549bba206511
cbaba20 <cbaba20@gmail.com>
2017-02-03 02:09:18 +0530

    Add project files.

---
+++ WebApplication12/Controllers/DefaultController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Data.Entity.Core.Objects;
+
+namespace WebApplication12.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public HttpResponseMessage get()
+        {
+
+
+
+
+            int count = 0;
+
+            Parallel.Invoke(() =>
+            {
+                testdbEntities tdb = new testdbEntities();
+                ObjectParameter obj1 = new ObjectParameter("count", typeof(int));
+
+                tdb.usp_FetchEmployeecount(obj1);
+
+                count += Convert.ToInt32(obj1.Value);
+
+            }, () =>
+            {
+                testdbEntities tdb = new testdbEntities();
+                ObjectParameter obj2 = new ObjectParameter("count", typeof(int));
+
+                tdb.usp_GetEmployeeCount(obj2);
+
+                count += Convert.ToInt32(obj2.Value);
+
+            });
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(count.ToString(), System.Text.Encoding.UTF8, "application/json")
+            };
+            return resp;
+
+
+        }
+    }
+}
